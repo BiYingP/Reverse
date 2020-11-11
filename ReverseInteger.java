1aee5b720f4910aa080f4f8d1ada7151b9e2bdc0
BiYing <bp483@drexel.edu>
2020-11-11 00:18:03 -0500

    added reverseInteger function

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,4 +1,23 @@
 package Reverse;
 
 public class ReverseInteger {
+
+    public int reverseInteger(int n){
+        long result = 0;
+        while(n != 0){
+            result = result * 10 + n % 10;
+            n = n / 10;
+        }
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int) result;
+    }
+
+    public static void main (String[] args){
+        ReverseInteger r = new ReverseInteger();
+        int n = 12345;
+        System.out.println(r.reverseInteger(n));
+
+    }
 }
